0a185fb4b1a4c5bb60fa181bac217d89f992d72c
Alexander Mazanyk <grejnext@gmail.com>
2022-10-06 00:35:34 +0300

    create second pages

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
-import React, {useState} from "react";
+import React from "react";
 import Input from "./components/Input/Input.jsx";
-import axios from "axios";
 import "./styles/App.css";
 
 
 function App() {
-  const [posts, setPosts] = useState([]);
-
-  async function fetchPosts() {
-    const response = await axios.get("https://jsonplaceholder.typicode.com/posts");
-    setPosts(response.data);
-  }
-
-  fetchPosts();
 
   return (
     <div className="App">
--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import MySelect from '../components/select/MySelect';
+import axios from "axios";
 const Posts = () => {
+
+    const [posts, setPosts] = useState([]);
+
+    async function fetchPosts() {
+        const response = await axios.get("https://jsonplaceholder.typicode.com/posts");
+        setPosts(response.data);
+    }
+
+    fetchPosts();
+
     return (
         <div>
+            <PostList posts={posts}></PostList>
             <MySelect
                 defaultValue="sort"
                 options={[
